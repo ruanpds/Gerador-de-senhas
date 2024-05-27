cfd640317b6efaa59e259dbb6b807075c063b48a
ruanpds <103281674+ruanpds@users.noreply.github.com>
2024-05-27 11:04:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+const numeroSenha = document.querySelectorAll('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho(){
+    if(tamanhoSenha <1){
+            tamanhoSenha--;
+    }
+numeroSenha.textContent = tamanhoSenha;
+}
